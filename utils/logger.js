ad4f57289a4c3f9da8f9c2438f85335ebd13edaa
CryptoBoyz <160847170+SRCryptoBoyz@users.noreply.github.com>
2025-01-04 05:57:51 +0000

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,10 +1,40 @@
 import chalk from 'chalk';
 
-const log = {
-    info: (...args) => console.log(chalk.blueBright('[INFO]'), ...args),
-    success: (...args) => console.log(chalk.greenBright('[SUCCESS]'), ...args),
-    warn: (...args) => console.log(chalk.yellow('[WARN]'), ...args),
-    error: (...args) => console.log(chalk.red('[ERROR]'), ...args),
+const logger = {
+    log: (level, message, value = '') => {
+        const now = new Date().toISOString();
+
+        const colors = {
+            info: chalk.green,
+            warn: chalk.yellow,
+            error: chalk.red,
+            success: chalk.blue,
+            debug: chalk.magenta,
+        };
+
+        const color = colors[level] || chalk.white;
+        const levelTag = `[ ${level.toUpperCase()} ]`;
+        const timestamp = `[ ${now} ]`;
+
+        const formattedMessage = `${chalk.green("[ Mygate-Node ]")} ${chalk.cyanBright(timestamp)} ${color(levelTag)} ${message}`;
+
+        let formattedValue = ` ${chalk.green(value)}`;
+        if (level === 'error') {
+            formattedValue = ` ${chalk.red(value)}`;
+        }
+        if (typeof value === 'object') {
+            const valueColor = level === 'error' ? chalk.red : chalk.green;
+            formattedValue = ` ${valueColor(JSON.stringify(value))}`;
+        }
+
+        console.log(`${formattedMessage}${formattedValue}`);
+    },
+
+    info: (message, value = '') => logger.log('info', message, value),
+    warn: (message, value = '') => logger.log('warn', message, value),
+    error: (message, value = '') => logger.log('error', message, value),
+    success: (message, value = '') => logger.log('success', message, value),
+    debug: (message, value = '') => logger.log('debug', message, value),
 };
 
-export default log;
+export default logger;
